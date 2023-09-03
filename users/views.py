f45470032508c81a47362ace109df0686975dc8e
Paw565pl <paw565pl@gmail.com>
2023-09-03 21:19:13 +0200

    feat(users.views.profile): added error message when does not meet valdiation criteria

--- users/views.py
+++ users/views.py
@@ -24,19 +24,25 @@ def register(request):
 
 @login_required()
 def profile(request):
+    user = request.user
     if request.method == "POST":
-        user_update_form = UserUpdateForm(request.POST, instance=request.user)
+        user_update_form = UserUpdateForm(request.POST, instance=user)
         profile_update_form = ProfileUpdateForm(
-            request.POST, request.FILES, instance=request.user.profile
+            request.POST, request.FILES, instance=user.profile
         )
         if user_update_form.is_valid() and profile_update_form.is_valid():
             user_update_form.save()
             profile_update_form.save()
             messages.success(request, f"Your account has been updated!")
             return redirect("profile")
+        else:
+            messages.error(
+                request, f"Provided data does not meet the requirements!", "danger"
+            )
+            return redirect("profile")
     else:
-        (profile, _) = Profile.objects.get_or_create(user=request.user)
-        user_update_form = UserUpdateForm(instance=profile)
+        (profile, _) = Profile.objects.get_or_create(user=user)
+        user_update_form = UserUpdateForm(instance=user)
         profile_update_form = ProfileUpdateForm(instance=profile)
         return render(
             request,
