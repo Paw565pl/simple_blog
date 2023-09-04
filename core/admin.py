cf7b3d7f69a410473586a4b7447039f770dc8614
Paw565pl <paw565pl@gmail.com>
2023-09-04 20:14:00 +0200

    fix(core): optimized user admin site

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import User
+from .forms import UserChangeForm
+
 
 # Register your models here.
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    form = UserChangeForm
---
+++ core/forms.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.forms import UserChangeForm as BaseUserChangeForm
+from .models import User
+
+
+class UserChangeForm(BaseUserChangeForm):
+    class Meta:
+        model = User
+        fields = "__all__"
