974c19b162782e49776886659678da1676a97fbe
Paw565pl <paw565pl@gmail.com>
2023-09-04 17:51:38 +0200

    fix(models): simplified models

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,8 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
+    content = models.TextField(max_length=1000)
+    last_update = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
