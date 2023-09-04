15c3efaceb74c4392604ebb5e03612583d01503a
Paw565pl <paw565pl@gmail.com>
2023-09-04 12:04:23 +0200

    feat(blog): created post detail view and template

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views as v
 
 urlpatterns = [
-    path("", v.home, name="blog-home"),
+    # path("", v.home, name="blog-home"),
+    path("", v.PostListView.as_view(), name="blog-home"),
+    path("post/<int:pk>/", v.PostDetailView.as_view(), name="post-detail"),
     path("about/", v.about, name="blog-about"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
 
 
-def home(request):
-    posts = Post.objects.all()
-    return render(request, "blog/home.html", {"posts": posts})
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/home.html"
+    context_object_name = "posts"
+    ordering = ["-date_posted"]
+
+
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = "post"
 
 
 def about(request):
