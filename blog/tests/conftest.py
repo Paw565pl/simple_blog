124d8b5ddb9a2d15d9fc3cdcd885b4bcd766c7b0
Paw565pl <paw565pl@gmail.com>
2023-09-11 16:27:02 +0200

    fix(blog.tests): fixed update tests and refactored others

--- blog/tests/conftest.py
+++ blog/tests/conftest.py
@@ -5,14 +5,6 @@ import pytest
 
 
 @pytest.fixture
-def posts():
-    def get_posts(author, quantity=10):
-        return baker.make(Post, author=author, _quantity=quantity)
-
-    return get_posts
-
-
-@pytest.fixture
 def get_post(client):
     def do_get_post(id):
         return client.get(f"/post/{id}/")
@@ -37,11 +29,11 @@ def create_post(client):
 
 
 @pytest.fixture
-def full_update_post(client):
-    def do_full_update_post(id: int, updated_post: Post):
-        return client.put(f"/post/{id}/edit/", updated_post)
+def update_post(client):
+    def do_update_post(id: int, updated_post: Post):
+        return client.post(f"/post/{id}/edit/", updated_post)
 
-    return do_full_update_post
+    return do_update_post
 
 
 @pytest.fixture
@@ -53,19 +45,27 @@ def delete_post(client):
 
 
 @pytest.fixture
-def user():
+def bake_posts():
+    def do_bake_posts(author, quantity=10):
+        return baker.make(Post, author=author, _quantity=quantity)
+
+    return do_bake_posts
+
+
+@pytest.fixture
+def bake_user():
     return baker.make(get_user_model())
 
 
 @pytest.fixture
-def users():
-    def get_users(quantity=2):
+def bake_users():
+    def do_bake_users(quantity=2):
         return [baker.make(get_user_model()) for _ in range(quantity)]
 
-    return get_users
+    return do_bake_users
 
 
 @pytest.fixture
-def authenticated_user(client, user):
-    client.force_login(user)
-    return user
+def authenticated_user(client, bake_user):
+    client.force_login(bake_user)
+    return bake_user
--- blog/tests/test_blog.py
+++ blog/tests/test_blog.py
@@ -1,14 +1,15 @@
 from random import randint
+from django.forms.models import model_to_dict
 from blog.models import Post
 import pytest
 
 
 @pytest.mark.django_db
 class TestRetrievePost:
-    def test_if_post_exists_returns_200(self, user, posts, get_post):
-        posts = posts(user)
+    def test_if_post_exists_returns_200(self, bake_user, bake_posts, get_post):
+        bake_posts = bake_posts(bake_user)
 
-        for post in posts:
+        for post in bake_posts:
             response = get_post(post.id)
 
             assert response.status_code == 200
@@ -20,10 +21,12 @@ class TestRetrievePost:
 
         assert response.status_code == 404
 
-    def test_if_user_posts_exist_returns_200(self, user, posts, get_posts_by_user):
-        posts = posts(user)
+    def test_if_user_posts_exist_returns_200(
+        self, bake_user, bake_posts, get_posts_by_user
+    ):
+        bake_posts = bake_posts(bake_user)
 
-        response = get_posts_by_user(user.username)
+        response = get_posts_by_user(bake_user.username)
 
         assert response.status_code == 200
 
@@ -33,63 +36,100 @@ class TestCreatePost:
     valid_data = {"title": "test", "content": "test"}
     invalid_data = {"title": "", "content": ""}
 
-    def test_if_user_is_anonymous_returns_302(self, create_post):
+    def test_if_user_is_anonymous_returns_login_view(self, create_post):
         response = create_post(self.valid_data)
 
         assert response.status_code == 302
         assert response.url == "/login/?next=/post/new/"
+        assert Post.objects.count() == 0
 
     def test_if_data_is_invalid_does_not_create(
         self,
         authenticated_user,
         create_post,
     ):
-        create_post(self.invalid_data)
+        response = create_post(self.invalid_data)
 
+        assert response.status_code == 200
         assert Post.objects.count() == 0
 
-    def test_if_data_is_valid_creates_post(self, authenticated_user, create_post):
-        create_post(self.valid_data)
+    def test_if_data_is_valid_creates(self, authenticated_user, create_post):
+        response = create_post(self.valid_data)
 
+        assert response.status_code == 302
+        assert response.url == "/"
         assert Post.objects.count() == 1
-        assert Post.objects.first().__dict__.items() >= self.valid_data.items()
+
+        created_post = Post.objects.first()
+
+        if created_post:
+            created_post_dict = model_to_dict(
+                created_post, fields=list(self.valid_data.keys())
+            )
+            assert created_post_dict == self.valid_data
 
 
 @pytest.mark.django_db
 class TestUpdatePost:
     def test_if_user_is_not_author_returns_403(
-        self, client, users, posts, full_update_post
+        self, client, bake_users, bake_posts, update_post
     ):
-        (user1, user2) = users()
-        (post,) = posts(user1, 1)
+        (user1, user2) = bake_users()
+        (post,) = bake_posts(user1, 1)
         client.force_login(user2)
 
         id = post.id
         updated_data = {"title": "test1", "content": "test1"}
-        response = full_update_post(id, updated_data)
+        response = update_post(id, updated_data)
 
         assert response.status_code == 403
+
+        post_dict = model_to_dict(
+            Post.objects.get(id=id), fields=list(updated_data.keys())
+        )
+        assert post_dict != updated_data
+
+    def test_if_data_is_valid_updates(
+        self, authenticated_user, bake_posts, update_post
+    ):
+        (post,) = bake_posts(authenticated_user, 1)
+
+        id = post.id
+        updated_data = {"title": "test1", "content": "test1"}
+        response = update_post(id, updated_data)
+
+        assert response.status_code == 302
+        assert response.url == "/post/23/"
+
+        updated_post_dict = model_to_dict(
+            Post.objects.get(id=id), fields=list(updated_data.keys())
+        )
+        assert updated_post_dict == updated_data
 
 
 @pytest.mark.django_db
 class TestDeletePost:
-    def test_if_user_is_not_author_returns_403(self, client, users, posts, delete_post):
-        (user1, user2) = users()
-        (post,) = posts(user1, 1)
+    def test_if_user_is_not_author_returns_403(
+        self, client, bake_users, bake_posts, delete_post
+    ):
+        (user1, user2) = bake_users()
+        (post,) = bake_posts(user1, 1)
         client.force_login(user2)
 
         id = post.id
         response = delete_post(id)
 
-        assert Post.objects.count() == 1
         assert response.status_code == 403
+        assert Post.objects.count() == 1
 
-    def test_if_data_is_valid_returns_302(self, authenticated_user, posts, delete_post):
-        (post,) = posts(authenticated_user, 1)
+    def test_if_data_is_valid_deletes(
+        self, authenticated_user, bake_posts, delete_post
+    ):
+        (post,) = bake_posts(authenticated_user, 1)
 
         id = post.id
         response = delete_post(id)
 
-        assert Post.objects.count() == 0
         assert response.status_code == 302
         assert response.url == "/"
+        assert Post.objects.count() == 0
