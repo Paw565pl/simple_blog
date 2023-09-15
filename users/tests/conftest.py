c318061b066791866e1b9fedf7c99a84eb9a6f81
Paw565pl <paw565pl@gmail.com>
2023-09-15 15:28:30 +0200

    fix(users.tests): moved captcha test keys to fixtures

--- users/tests/conftest.py
+++ users/tests/conftest.py
@@ -4,6 +4,7 @@ import pytest
 @pytest.fixture
 def register_user(client):
     def do_register_user(user_data):
+        user_data["h-captcha-response"] = "10000000-aaaa-bbbb-cccc-000000000001"
         return client.post("/register/", user_data)
 
     return do_register_user
@@ -12,7 +13,14 @@ def register_user(client):
 @pytest.fixture
 def login_user(client):
     def do_login_user(username, password):
-        return client.post("/login/", {"username": username, "password": password})
+        return client.post(
+            "/login/",
+            {
+                "username": username,
+                "password": password,
+                "h-captcha-response": "10000000-aaaa-bbbb-cccc-000000000001",
+            },
+        )
 
     return do_login_user
 
